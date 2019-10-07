ded01edeecd445f3e84c9d309a4de3321fa8ccdf
DanielJorn <0954264@gmail.com>
2019-10-07 21:31:38 +0300

    Pushed to GitHub 2.

--- src/main/java/com/codenjoy/dojo/battlecity/handler/MainHandler.java
+++ src/main/java/com/codenjoy/dojo/battlecity/handler/MainHandler.java
@@ -7,7 +7,6 @@ import com.codenjoy.dojo.battlecity.client.Board;
  * This class delegates the deciding whether action is the best for a given board to various handlers, and makes that action
  */
 public class MainHandler implements IMainHandler {
-
     @Override
     public String handle(Board nowTable) {
         return new MoveActions().up();
